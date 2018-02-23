33324fc0e6cb8e7e915324a8d45e709322c96ce1
Lorenzo Selvatici <lselvati@ucsc.edu>
2018-02-22 19:31:53 -0800

    Got password, need to test pipeline.py

---
+++ phase1/pipeline.py
@@ -0,0 +1,172 @@
+from scapy.all import *
+from crypt import crypt
+
+import zipfile
+import io
+
+import sys
+import socket
+from datetime import datetime
+
+from time import sleep
+
+from binascii import unhexlify
+
+NSA_SERVER = ('128.114.59.42', 2001)
+# NSA_SERVER = ('127.0.0.1', 41257)
+
+try:
+    CRUZID = sys.argv[1]
+except:
+    print('Pass CRUZID as first parameter')
+    exit(1)
+
+def get_payload(pcap):
+    return rdpcap(pcap)[0].load
+
+
+def get_crypted_passwd(passwd_pcap):
+    crypted_passwd = get_payload(passwd_pcap)
+    return crypted_passwd.decode().strip('\n')
+
+
+def open_listen_socket(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('', port))
+    sock.listen(1)
+    return sock, sock.getsockname() ## return the (ip, port)
+
+
+def send_crack_passwd_req(crypted_passwd, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        
+        while sock.connect_ex(NSA_SERVER) != 0:
+            sleep(1)
+
+        req = "%s 128.114.59.29 %d" % (crypted_passwd, port)
+        print('Sending "%s"' % req)
+        
+        sock.send(req.encode())
+        
+        reply = sock.recv(10)
+        print(reply)
+
+
+def print_time():
+    now = datetime.now().time()
+    print("\n------ %d:%d:%d" % (now.hour, now.minute, now.second))
+
+
+def crack_passwd(crypted_passwd):
+    listen_sock, (ip, port) = open_listen_socket(51245)
+
+    print_time()
+    send_crack_passwd_req(crypted_passwd, port)
+    
+    sock, _ = listen_sock.accept()
+
+    reply = sock.recv(100).decode()
+    
+    listen_sock.close()
+    sock.close()
+
+    ## reply =  "<crypted_password> <passwd>\n"
+    _, passwd = reply.strip('\n').split(' ')
+
+    print('Received passwd = "%s"' % passwd)
+
+    return passwd
+
+
+def get_obfkey(key_payload, passwd):
+    try:
+        passwd = passwd.encode()
+    except:
+        pass
+    with zipfile.ZipFile(io.BytesIO(key_payload)) as archive:
+        with archive.open('key', pwd=passwd) as key_file:
+            key_file_content = key_file.read().decode().strip('\n')
+            return key_file_content
+
+
+def possible_keys(obfkey):
+    obfkey = obfkey.strip('\n')
+    for s in range(0, len(obfkey)-31, 1):
+        yield obfkey[s:s+32]
+
+
+def decrypt_ciphertext(ciphertext, key, iv):
+    cipherfile = 'tmp/ciphertext'
+    textfile   = 'tmp/plaintext'
+
+    with open(cipherfile, 'wb') as ct:
+        ct.write(ciphertext)
+
+    cmd = './decrypt %s %s %s' % (key, iv, cipherfile)
+    proc = subprocess.run(cmd)
+    assert(proc.returncode == 0)
+
+    with open(textfile, 'rt') as pt:
+        return pt.read()
+
+
+def load_dict():
+    with open('dictionary.txt', 'rt') as d:
+        words = [line.lower() for line in d.readlines()]
+        print('Loaded dictionary of %d words' % len(words))
+        return set(words)
+
+
+def makes_sense(msg):
+    # dictionary = ['fluffy', 'fiona', 'unicorns', 'capitol', 'hill', 'bob', 'fiona', 'london', 'bbc', 'washington']
+    dictionary = load_dict()
+    words = msg.lower().split(' ')
+    
+    count = 0
+
+    for word in words:
+        if word in dictionary:
+            count += 1
+
+    min_hit_number = 0.5 * len(words)
+    return count >= min_hit_number
+
+
+def get_message(ciphertext, obfkey, iv):
+
+    for key in possible_keys(obfkey):
+        decrypted_msg = decrypt_ciphertext(ciphertext, key, iv)
+
+        if makes_sense(decrypted_msg):
+            return decrypted_msg
+
+
+def main():
+    passwd_pcap  = 'packets/%s.passwd.pcap'  % CRUZID
+    keyzip_pcap  = 'packets/%s.key.zip.pcap' % CRUZID
+    message_pcap = 'packets/%s.message.pcap' % CRUZID
+    iv_pcap      = 'packets/%s.iv.pcap'      % CRUZID
+
+    # crypted_passwd = get_crypted_passwd(passwd_pcap)
+    # print('crypted_passwd', crypted_passwd)
+
+    # passwd = crack_passwd(crypted_passwd)
+    # print('passwd', passwd)
+
+    passwd = "dTRrcA"
+
+    obfkey = get_obfkey(get_payload(keyzip_pcap), passwd)
+    print('obfkey', obfkey)
+
+    iv = unhexlify(get_payload(iv_pcap).strip(b'\n'))
+    print('iv', iv)
+
+    ciphertext = get_payload(message_pcap)
+
+    message = get_message(ciphertext, obfkey, iv)
+    print('MESSAGE')
+    print(message)
+
+
+
+main()
--- phase1/scapy-test/read_pcap.py
+++ phase1/scapy-test/read_pcap.py
@@ -4,6 +4,10 @@ from crypt import crypt
 import zipfile
 import io
 
+from binascii import unhexlify
+
+from Crypto.Cipher import AES
+
 # rdpcap comes from scapy and loads in our pcap file
 passwd_packet = rdpcap('../passwd.pcap')[0]
 keyzip_packet = rdpcap('../key.zip.pcap')[0]
@@ -17,6 +21,7 @@ def real_key():
     with open('../key', 'r') as k:
         return k.read().strip('\n')
 
+
 def get_obfkey(key_payload, passwd):
     try:
         passwd = passwd.encode()
@@ -29,18 +34,48 @@ def get_obfkey(key_payload, passwd):
             
 
 crypted_passwd = get_payload(passwd_packet)
-print(crypted_passwd)
+# print(crypted_passwd)
 
 salt = crypted_passwd[:2]
 
 crypted_fluffy = crypt('fluffy', salt)
-print(crypted_fluffy)
+# print(crypted_fluffy)
 
 
 assert get_obfkey(keyzip_packet.load, 'fluffy') == real_key()
 
 ## TODO install pycrypto for AES encryption
 
+def get_file_content(filename):
+    with open(filename, 'r') as f:
+        return f.read().strip('\n')
+
+iv = unhexlify(iv_packet.load[:-1])
+
+# print('iv len(%d)' % len(iv), iv)
+
+keystr = get_file_content('../key.plain')
+# print('keystr', keystr)
+key = unhexlify(keystr)
+# print('key len(%d)' % len(key), key)
+
+
+aes1 = AES.new(key, AES.MODE_CBC, iv)
+
+hellocipher1 = aes1.encrypt(b"CMPS122 is an awesome class!    ")
+
+aes2 = AES.new(key, AES.MODE_CBC, iv)
+
+hellocipher2 = aes2.encrypt(b"CMPS122 is an awesome class!\x1c\x1c\x1c\x1c")
+
+print('1', hellocipher1)
+print('2', hellocipher2)
+
+# aes = AES.new(key, AES.MODE_CBC, iv)
+
+# hello = aes.decrypt(hellocipher)
+
+# print(hello)
 
 
 
