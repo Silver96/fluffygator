f2de8376ab363b139fa3f2d3a392aeae4a6a64f9
Andrea Silvestroni <96andrea9642@gmail.com>
2018-03-07 00:07:05 -0800

    Fixed third cipher [PP]

--- phase3/ciphers/mistery_cipher.py
+++ phase3/ciphers/mistery_cipher.py
@@ -3,75 +3,41 @@ import os
 import sys
 
 def generate_dictionary():
-    test_plaintext = [
-    "Is it thy will thy image should keep open",
-    "My heavy eyelids to the weary night",
-    "Dost thou desire my slumbers should be broken",
-    "While shadows like to thee do mock my sight",
-    "Is it thy spirit that thou sendst from thee",
-    "So far from home into my deeds to pry",
-    "To find out shames and idle hours in me",
-    "The scope and tenure of thy jealousy",
-    "O no thy love though much is not so great",
-    "It is my love that keeps mine eye awake",
-    "Mine own true love that doth my rest defeat",
-    "To play the watchman ever for thy sake",
-    "  For thee watch I whilst thou dost wake elsewhere",
-    "  From me far off with others all too near"
-    ]
+    dictionary = ["a b c d e",
+                  "f g h i j",
+                  "k l m n o",
+                  "p q r s t",
+                  "u v w x y"]
 
-    test_plaintext = "\n".join(test_plaintext)
+    dictionary = [x.split(" ") for x in dictionary]
 
     key_dict = {}
 
-    test_ciphertext = ""
+    for i in range(5):
+        for j in range(5):
+            key_dict["%d%d" % (i+1,j+1)] = dictionary[i][j]
 
-    with open("ciphers/cipher3", "rt") as file:
-        for line in file.readlines():
-            # line = line.strip(" \n")
-            test_ciphertext += line
-
-    for i, c in enumerate(test_plaintext):
-        c = c.lower()
-        pair = test_ciphertext[2*i:2*i+2]
-        # print(pair)
-        if pair in key_dict:
-            # print(pair, c, key_dict)
-            assert key_dict[pair] == c
-        else:
-            key_dict[pair] = c
-
-    for i in range(0, len(test_ciphertext), 2):
-        pair = test_ciphertext[i:i+2]
-        # print(key_dict[pair], end="")
-
-    for i in range(10):
-        key_dict[" %d" % i] = str(i)
-
-    # key_dict["\n "] = "\n"
-
-    with open("ciphers/key_dict", "wt") as file:
-        json.dump(key_dict, file)
+    return key_dict
 
 def decipher_plaintext(ciphertext, key_dict):
     result = ""
     try:
         for i in range(0, len(ciphertext), 2):
             pair = ciphertext[i:i+2]
-            result += key_dict[pair]
+            if pair in key_dict:
+                result += key_dict[pair]
+            elif pair == "  ":
+                result += " "
+            else:
+                result += pair.strip(" ")
 
         return result
+
     except Exception as e:
         print("Exception", e)
         return None
 
 
 def mistery(cipher):
-    if not os.path.isfile("ciphers/key_dict"):
-        generate_dictionary()
-
-    key_dict = None
-    with open("ciphers/key_dict", "rt") as file:
-        key_dict = json.load(file)
-
+    key_dict = generate_dictionary()
     return decipher_plaintext(cipher, key_dict)
--- phase3/single_pipeline.py
+++ phase3/single_pipeline.py
@@ -6,35 +6,21 @@ import subprocess
 FAILED_LIST = "failed"
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: %s dir_name start_num" % sys.argv[0])
+    if len(sys.argv) < 3:
+        print("Usage: %s dir_name msg_count" % sys.argv[0])
         exit(1)
 
     students_dir = sys.argv[1]
-    start = 0
+    msg_count = int(sys.argv[2])
 
+    with open(FAILED_LIST, "wt") as f_failed:
 
-    if len(sys.argv) == 3:
-        start = int(sys.argv[2])
-
-    with open(FAILED_LIST, "at") as f_failed:
-
-        for student_id in range(start, len(os.listdir(students_dir))):
+        for student_id in range(len(os.listdir(students_dir))):
             student_dir = "%s/%d/" % (students_dir, student_id)
-            passwd_file = student_dir + "/passwd.plain"
-
-            passwd = ""
-
-            if os.path.isfile(passwd_file):
-                with open(passwd_file, "rt") as file:
-                    passwd = file.read()
-
-
-            cmd = "python3 pipeline.py %s %s" % (student_dir, passwd)
-            # print(cmd)
-
-            # subprocess.Popen(cmd.split(' '))
-            if subprocess.run(cmd.split(' ')).returncode == 2:
+            
+            cmd = "python3 pipeline.py %s %d" % (student_dir, msg_count)
+            
+            if subprocess.run(cmd.split(' ')).returncode != 0:
                 f_failed.write(str(student_id))
                 f_failed.write("\n")
                 f_failed.flush()
