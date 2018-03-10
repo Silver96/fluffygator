35a4fc1b24066deffacace0094aa5c38ada57304
Lorenzo Selvatici <lselvati@ucsc.edu>
2018-03-09 16:51:53 -0800

    Updated submit.sh

--- submit.sh
+++ submit.sh
@@ -8,8 +8,14 @@ fi
 
 
 paths=$(grep -rn $USER $1 | cut -f1 -d':')
-files=$(for p in $paths; do basename $p; done)
 
-cp $paths .
-tar czvf CMPS122-Lab3-Phase4.tar.gz $files
-cp CMPS122-Lab3-Phase4.tar.gz ~/
+i=0
+files=""
+for p in $paths; do 
+    i=$(($i + 1))
+    cp $p plain$i
+    files="$files plain$i"
+done
+
+tar czvf CMPS122-Lab3-Phase3.tar.gz $files
+# cp CMPS122-Lab3-Phase3.tar.gz ~/
